55f1a03b7ff0d272f9f6774b3b3d5523412abf43
Mugtaba <m_subahi@hotmail.com>
2022-07-07 03:16:32 +0100

    removed url log

--- src/helpers/getPrayerTimes.ts
+++ src/helpers/getPrayerTimes.ts
@@ -1,7 +1,6 @@
 import { IApi } from '../interfaces';
 
 export default async (url: string): Promise<IApi> => {
-  console.log('GET ', url);
   const response: Response = await fetch(url);
   return response.json();
 };
